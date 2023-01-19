29ff4ec47b594c5a90bb1467da54bfd05289dcfd
Daniel Ives <dives@qa.com>
2023-01-19 16:06:24 +0000

    tidied up dep demo

--- deployments/update_deployment.sh
+++ deployments/update_deployment.sh
@@ -3,14 +3,14 @@
 . ../demo-magic.sh
 clear
 
-pe "kubectl set image deployments.app/simple-app simple-app=danielives/helloworld:v2"
+pe "kubectl set image deployments.app/simple-app simple-app=danielives/simple-app:v2"
 pei "kubectl rollout pause deployments.app/simple-app"
 
 pe "kubectl rollout resume deployments.app/simple-app"
 
 pe "kubectl rollout history deployments.app/simple-app"
 
-pe "kubectl set image deployments.app/simple-app simple-app=danielives/helloworld:v3 --record"
+pe "kubectl set image deployments.app/simple-app simple-app=danielives/simple-app:v3 --record"
 
 pe "kubectl rollout history deployments.app/simple-app"
 
